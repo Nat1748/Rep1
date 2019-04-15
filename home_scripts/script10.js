65d8cedb219ef106b26045c10bff713cd137b21c
Nat1748 <49454263+Nat1748@users.noreply.github.com>
2019-04-15 18:02:49 +0300

    new

--- home_scripts/script10.js
+++ home_scripts/script10.js
@@ -1,4 +1,4 @@
 var money = +prompt("Введіть кількість монет")
 var bill = parseInt(money / 100)
-var coin = money - bill * 100 
+var coin = money % 100
 document.write(bill +"грн"+coin+"коп")
--- home_scripts/script8.js
+++ home_scripts/script8.js
@@ -1,12 +1,3 @@
 var year = +prompt("Вкажіть рік", "0000")
-function isCentury(year){
-return year % 100 === 0;
-}
-if(isCentury(year)) {
-    let c = parseInt(year / 100);
-document.write("вказаний рік це століття" + c);
-} 
-else{
-    let d = parseInt(year / 100) + 1;
-    document.write("вказаний рік це століття" + d);
-}
+let c = parseInt((year -1) / 100 + 1);
+document.write(c)
